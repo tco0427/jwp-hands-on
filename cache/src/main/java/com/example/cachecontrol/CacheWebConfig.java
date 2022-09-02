6752cd51a6758be8e5c1ffc95a7421270b48135b
Donggyu <a29661498@gmail.com>
2022-09-02 19:52:32 +0900

    feat: 캐시 무효화(Cache Busting)

--- cache/src/main/java/com/example/cachecontrol/CacheWebConfig.java
+++ cache/src/main/java/com/example/cachecontrol/CacheWebConfig.java
@@ -1,5 +1,8 @@
 package com.example.cachecontrol;
 
+import static com.example.version.CacheBustingWebConfig.PREFIX_STATIC_RESOURCES;
+
+import com.example.version.ResourceVersion;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,8 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CacheWebConfig implements WebMvcConfigurer {
 
+    private final ResourceVersion version;
+
+    public CacheWebConfig(final ResourceVersion version) {
+        this.version = version;
+    }
+
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
-        registry.addInterceptor(new CacheInterceptor());
+        registry.addInterceptor(new CacheInterceptor())
+                .excludePathPatterns(PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/**");
     }
 }
