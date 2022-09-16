230f99b8d907638f6c0b28f9eec45923263178de
Donggyu <a29661498@gmail.com>
2022-09-16 15:42:59 +0900

    feat: ReflectionsTest

--- reflection/src/test/java/reflection/ReflectionsTest.java
+++ reflection/src/test/java/reflection/ReflectionsTest.java
@@ -1,5 +1,9 @@
 package reflection;
 
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -14,5 +18,12 @@ class ReflectionsTest {
         Reflections reflections = new Reflections("examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        final Set<Class<?>> annotatedWithController = reflections.getTypesAnnotatedWith(Controller.class);
+        final Set<Class<?>> annotatedWithService = reflections.getTypesAnnotatedWith(Service.class);
+        final Set<Class<?>> annotatedWithRepository = reflections.getTypesAnnotatedWith(Repository.class);
+
+        log.info("{}", annotatedWithController);
+        log.info("{}", annotatedWithService);
+        log.info("{}", annotatedWithRepository);
     }
 }
