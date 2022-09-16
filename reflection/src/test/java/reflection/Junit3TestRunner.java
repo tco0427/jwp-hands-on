ad0d53b1b1d5526bbb88d16653364534ac0d1c8e
Donggyu <a29661498@gmail.com>
2022-09-16 15:02:13 +0900

    feat: JUnit3TestRunner

--- reflection/src/test/java/reflection/Junit3TestRunner.java
+++ reflection/src/test/java/reflection/Junit3TestRunner.java
@@ -1,5 +1,7 @@
 package reflection;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
@@ -7,7 +9,15 @@ class Junit3TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        final Constructor<Junit3Test> constructor = clazz.getConstructor();
+        final Junit3Test node = constructor.newInstance();
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        final Method[] methods = clazz.getMethods();
+        for (Method method : methods) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(node);
+            }
+        }
     }
 }
