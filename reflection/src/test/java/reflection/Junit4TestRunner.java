3fa875ff1e1ecb6229a6d74d25d5ea96fcd1cf7a
Donggyu <a29661498@gmail.com>
2022-09-16 15:08:55 +0900

    feat: JUnit4TestRunner

--- reflection/src/test/java/reflection/Junit4TestRunner.java
+++ reflection/src/test/java/reflection/Junit4TestRunner.java
@@ -1,5 +1,7 @@
 package reflection;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
@@ -7,7 +9,15 @@ class Junit4TestRunner {
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        final Constructor<Junit4Test> constructor = clazz.getConstructor();
+        final Junit4Test node = constructor.newInstance();
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        final Method[] methods = clazz.getMethods();
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(MyTest.class)) {
+                method.invoke(node);
+            }
+        }
     }
 }
