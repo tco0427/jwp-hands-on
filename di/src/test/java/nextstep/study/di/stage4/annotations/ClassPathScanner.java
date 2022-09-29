e6e2bc55e956f49ec4e33db370b8ed7f15cc259c
Donggyu <a29661498@gmail.com>
2022-09-29 09:24:59 +0900

    feat: Stage4 Test (DIContainer 구현)

--- di/src/test/java/nextstep/study/di/stage4/annotations/ClassPathScanner.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,23 @@
 package nextstep.study.di.stage4.annotations;
 
+import java.util.HashSet;
 import java.util.Set;
+import org.reflections.Reflections;
 
 public class ClassPathScanner {
 
     public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+        Set<Class<?>> classes = new HashSet<>();
+        Reflections reflections = new Reflections(packageName);
+
+        final Set<Class<?>> repositoryClasses = reflections.getTypesAnnotatedWith(Repository.class);
+        final Set<Class<?>> serviceClasses = reflections.getTypesAnnotatedWith(Service.class);
+        final Set<Class<?>> injectClasses = reflections.getTypesAnnotatedWith(Inject.class);
+
+        classes.addAll(repositoryClasses);
+        classes.addAll(serviceClasses);
+        classes.addAll(injectClasses);
+
+        return classes;
     }
 }
--- di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
@@ -1,5 +1,9 @@
 package nextstep.study.di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -10,15 +14,65 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = createBeans(classes);
+        this.beans.forEach(this::setFields);
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        final Set<Class<?>> allClassesInPackage = ClassPathScanner.getAllClassesInPackage(rootPackageName);
+        return new DIContainer(allClassesInPackage);
+    }
+
+    private Set<Object> createBeans(final Set<Class<?>> classes) {
+        Set<Object> beans = new HashSet<>();
+        for (Class<?> aClass : classes) {
+            beans.add(createInstance(aClass));
+        }
+        return beans;
+    }
+
+    private static Object createInstance(final Class<?> aClass) {
+        try {
+            final Constructor<?> constructor = aClass.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        } catch(InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    private void setFields(final Object bean) {
+        final Field[] fields = bean.getClass().getDeclaredFields();
+
+        for (Field field : fields) {
+            setBeanField(bean, field);
+        }
+    }
+
+    private void setBeanField(final Object bean, final Field field) {
+        try {
+            injectField(bean, field);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void injectField(final Object bean, final Field field) throws IllegalAccessException {
+        field.setAccessible(true);
+        if (hasInjectAnnotation(field)) {
+            field.set(bean, getBean(field.getType()));
+        }
+    }
+
+    private boolean hasInjectAnnotation(final Field field) {
+        return field.isAnnotationPresent(Inject.class);
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(bean -> aClass.isAssignableFrom(bean.getClass()))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
     }
 }
